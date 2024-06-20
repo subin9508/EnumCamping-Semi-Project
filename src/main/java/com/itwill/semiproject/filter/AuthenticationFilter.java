910da2fad64e34a552d7e1a507ba812209eaf2e3
ADMIN <ADMIN@DESKTOP-JDFSTM9>
2024-06-20 13:57:02 +0900

    Merge branch 'develop' of https://github.com/woodstock0513/semiproject.git into develop

--- src/main/java/com/itwill/semiproject/filter/AuthenticationFilter.java
+++ src/main/java/com/itwill/semiproject/filter/AuthenticationFilter.java
@@ -11,6 +11,7 @@ import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class AuthenticationFilter extends HttpFilter {
