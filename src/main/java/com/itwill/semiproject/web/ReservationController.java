8c2fc35e9f3ea93f06e3fb0afffb882490eb053d
LIMGEONHEE <dlarjsgml321@naver.com>
2024-07-01 18:20:24 +0900

    7/1

---
+++ src/main/java/com/itwill/semiproject/web/ReservationController.java
@@ -0,0 +1,20 @@
+package com.itwill.semiproject.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/reservation")
+public class ReservationController {
+
+	@GetMapping("/item")
+	public String item() {
+		log.debug("/item");
+		
+		return "/reservation/item";			
+	}
+}
