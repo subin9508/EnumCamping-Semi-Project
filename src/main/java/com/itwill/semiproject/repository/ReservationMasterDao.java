a8052274f836a5537bc9ba9eada7bd4687bae76a
woodstock0513 <judydayoung@gmail.com>
2024-07-10 14:16:54 +0900

    만드는중

--- src/main/java/com/itwill/semiproject/repository/ReservationMasterDao.java
+++ src/main/java/com/itwill/semiproject/repository/ReservationMasterDao.java
@@ -10,6 +10,7 @@ import com.itwill.semiproject.dto.ReservationListDto;
 public interface ReservationMasterDao {
 
 	// 예약 id로 예약 정보를 조회
+	List<ReservationListDto> selectByUserId (String userId);
 	ReservationMaster selectByResId(Integer resId);
 	List<Integer> selectByResCheckIn(@Param("resCheckIn") String resCheckIn);
 	List<ReservationMaster> selectByItemIdAndResCheckIn(@Param("itemId") int itemId, @Param("resCheckIn") String resCheckIn);
