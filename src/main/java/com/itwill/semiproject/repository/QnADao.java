c81116660dfa3002d339a01594b451f34706696d
katekim <kms252502@naver.com>
2024-07-15 15:19:37 +0900

    240715 QnA 최종

--- src/main/java/com/itwill/semiproject/repository/QnADao.java
+++ src/main/java/com/itwill/semiproject/repository/QnADao.java
@@ -29,6 +29,7 @@ public interface QnADao {
     
     long selectTotalCount(QnASearchDto dto);  // 검색 조건에 맞는 전체 글의 갯수 조회
 
-  int updateQnaState(Integer qnaPostId, Integer qnaState);
-
+    int updateQnaState(Integer qnaPostId, Integer qnaState);
+  
+    List<QnA> selectQnAByUserId(String userId);
 }
